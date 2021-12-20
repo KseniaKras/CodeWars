83e125982ffdc3b276085c13e1735b76c642fd21
unknown <ksenia.kras29@gmail.com>
2021-12-20 23:07:07 +0300

    added new file

---
+++ kata-basic-math-operations.js
@@ -0,0 +1,16 @@
+// 8 kyu
+// Basic Mathematical Operations
+
+function basicOp(operation, value1, value2)
+{
+    if(operation === "+"){
+      return value1 + value2;
+    } else if(operation === "-") {
+      return value1 - value2;
+    } else if(operation === "*") {
+      return value1 * value2;
+    } else if(operation === "/") {
+      return value1 / value2;
+    }
+  // Code
+}
