33ba334f393f8ffeaf0e09fd561497b2d1b44af4
unknown <ksenia.kras29@gmail.com>
2021-12-21 20:29:22 +0300

    added new file kata-convert-boolean-strings.js

---
+++ kata-convert-boolean-strings.js
@@ -0,0 +1,11 @@
+// 8 kyu
+// Convert boolean values to strings 'Yes' or 'No'.
+
+function boolToWord( bool ){
+   if (bool === true) {
+     return String("Yes");
+   } else {
+     return String('No')
+   }
+    //...
+}
